fa4c947beb5599be0b991ae16065c72b6184e657
Mohsin Ali <59621582+Mohsin-4@users.noreply.github.com>
2025-08-19 15:06:27 +0500

    .

--- people.js
+++
@@ -1,9 +0,0 @@
-const people = ["mohsin", "saad", "ali", "khan"];
-const ages = [30, 20, 12, 34];
-console.log(people);
-
-
-module.exports = {
-    people, ages
-
-}
--- server.js
+++ server.js
@@ -1,11 +1,51 @@
-// setTimeout(()=>{
-// console.log("workinggg");
-// }, 3000);
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method);
 
 
-// const init = setInterval(()=>{
-//     console.log("interval working");
-// }, 1000)
+// set header content type
+    res.setHeader('Content-Type', 'text/html');
+    
+    let path = './files/views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200; 
+            break;
+        case '/test':
+            path += 'index.html';
+            res.statusCode = 200; 
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200; 
+            break;
+        case '/blah':
+            path += 'about.html';
+            res.statusCode = 200; 
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404; 
+            break;
+    }
 
-console.log(__dirname);
-console.log(__filename);
+    // fs.readFile('./files/views/index.html',(err, data)=>{
+    fs.readFile(path,(err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    })
+    // res.write('<p>hello Mohsin</p>');
+    // res.write('<p>hello again, Mohsin Ali</p>');
+    // res.end();
+});
+
+server.listen(3000, 'localhost', ()=>{
+    console.log('listening for request on port 3000');
+})
